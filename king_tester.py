df12bff9174b621ae253eb3575e2e8a51a8fa5fb
ian-miller1 <42287797+ian-miller1@users.noreply.github.com>
2019-06-15 01:57:07 -0700

    fixed

--- king_tester.py
+++ king_tester.py
@@ -1,4 +1,36 @@
-<<<<<<< HEAD
+import csv
+
+class Game:
+    def __init__(self, date, home_team_city, away_team_city, home_moneyLine, away_moneyLine):
+        self.date = date
+        self.home_team_city = home_team_city
+        self.away_team_city = away_team_city
+        self.home_moneyLine = home_moneyLine
+        self.away_moneyLine = away_moneyLine
+    
+listOfGames = []
+
+with open('C:/Users/imaxm/Desktop/mitSophomore/sportsBetting/historical_odds/1617.csv') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=",")
+    line_count = 1
+
+    for row in csv_reader:
+        if line_count == 1:
+            print(row)
+            line_count += 1
+        else:
+
+            if line_count % 2 == 0:
+                away_team_city = row[3]
+                away_moneyLine = row[11]
+                line_count += 1
+            else:
+                home_team_city = row[3]
+                home_moneyLine = row[11]
+                line_count += 1
+                newGame = Game(row[0], home_team_city, away_team_city, home_moneyLine, away_moneyLine)
+                listOfGames.append(newGame)
+
 import csv
 
 class Game:
@@ -32,39 +64,4 @@ with open('C:/Users/imaxm/Desktop/mitSophomore/sportsBetting/historical_odds/161
                 newGame = Game(row[0], home_team_city, away_team_city, home_moneyLine, away_moneyLine)
                 listOfGames.append(newGame)
     
-=======
-import csv
-
-class Game:
-    def __init__(self, date, home_team_city, away_team_city, home_moneyLine, away_moneyLine):
-        self.date = date
-        self.home_team_city = home_team_city
-        self.away_team_city = away_team_city
-        self.home_moneyLine = home_moneyLine
-        self.away_moneyLine = away_moneyLine
-    
-listOfGames = []
-
-with open('C:/Users/imaxm/Desktop/mitSophomore/sportsBetting/historical_odds/1617.csv') as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=",")
-    line_count = 1
-
-    for row in csv_reader:
-        if line_count == 1:
-            print(row)
-            line_count += 1
-        else:
-
-            if line_count % 2 == 0:
-                away_team_city = row[3]
-                away_moneyLine = row[11]
-                line_count += 1
-            else:
-                home_team_city = row[3]
-                home_moneyLine = row[11]
-                line_count += 1
-                newGame = Game(row[0], home_team_city, away_team_city, home_moneyLine, away_moneyLine)
-                listOfGames.append(newGame)
-    
->>>>>>> 245af6fa49005f44a8da7692ab9e20472b55ab21
-print(listOfGames[0].home_moneyLine)
+print("home line for 1st game:", listOfGames[0].home_moneyLine)
