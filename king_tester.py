83c12ac3c96042749867ca5c4c301b26b367cd9c
ian-miller1 <42287797+ian-miller1@users.noreply.github.com>
2019-07-14 13:41:06 -0700

    tester progress

--- king_tester.py
+++ king_tester.py
@@ -10,7 +10,7 @@ class Game:
     
 listOfGames = []
 
-with open('C:/Users/imaxm/Desktop/mitSophomore/sportsBetting/historical_odds/1617.csv') as csv_file:
+with open('C:/Users/imaxm/Desktop/mitSophomore/sportsBetting/miller-fund/historical_odds/1617.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=",")
     line_count = 1
 
@@ -31,37 +31,6 @@ with open('C:/Users/imaxm/Desktop/mitSophomore/sportsBetting/historical_odds/161
                 newGame = Game(row[0], home_team_city, away_team_city, home_moneyLine, away_moneyLine)
                 listOfGames.append(newGame)
 
-import csv
 
-class Game:
-    def __init__(self, date, home_team_city, away_team_city, home_moneyLine, away_moneyLine):
-        self.date = date
-        self.home_team_city = home_team_city
-        self.away_team_city = away_team_city
-        self.home_moneyLine = home_moneyLine
-        self.away_moneyLine = away_moneyLine
     
-listOfGames = []
-
-with open('C:/Users/imaxm/Desktop/mitSophomore/sportsBetting/historical_odds/1617.csv') as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=",")
-    line_count = 1
-
-    for row in csv_reader:
-        if line_count == 1:
-            print(row)
-            line_count += 1
-        else:
-
-            if line_count % 2 == 0:
-                away_team_city = row[3]
-                away_moneyLine = row[11]
-                line_count += 1
-            else:
-                home_team_city = row[3]
-                home_moneyLine = row[11]
-                line_count += 1
-                newGame = Game(row[0], home_team_city, away_team_city, home_moneyLine, away_moneyLine)
-                listOfGames.append(newGame)
-    
-print("home line for 1st game:", listOfGames[0].home_moneyLine)
+print("home line for 1st game is", listOfGames[0].home_moneyLine)
