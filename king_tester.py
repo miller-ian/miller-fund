6c1d3ed81094350ac209977a468a80d03a29b754
ian-miller1 <42287797+ian-miller1@users.noreply.github.com>
2019-10-03 01:21:28 -0400

    versions stabilized

--- king_tester.py
+++ king_tester.py
@@ -3,10 +3,6 @@ import csv
 import requests
 from lxml import html
 
-<<<<<<< HEAD
-=======
-
->>>>>>> d21ded2dc00af072fceb105101f5f3ef51d1dee5
 class Game:
     def __init__(self, date, home_team_city, away_team_city, home_moneyLine, away_moneyLine):
         self.date = date
@@ -14,35 +10,6 @@ class Game:
         self.away_team_city = away_team_city
         self.home_moneyLine = home_moneyLine
         self.away_moneyLine = away_moneyLine
-<<<<<<< HEAD
-    
-listOfGames = []
-
-with open('C:/Users/imaxm/Desktop/mitSophomore/sportsBetting/miller-fund/historical_odds/1617.csv') as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=",")
-    line_count = 1
-
-    for row in csv_reader:
-        if line_count == 1:
-            print(row)
-            line_count += 1
-        else:
-
-            if line_count % 2 == 0:
-                away_team_city = row[3]
-                away_moneyLine = row[11]
-                line_count += 1
-            else:
-                home_team_city = row[3]
-                home_moneyLine = row[11]
-                line_count += 1
-                newGame = Game(row[0], home_team_city, away_team_city, home_moneyLine, away_moneyLine)
-                listOfGames.append(newGame)
-
-
-    
-print("home line for 1st game is", listOfGames[0].home_moneyLine)
-=======
 
 year = str(input("Enter year:"))
 team = input("Enter 3 letter abbreviation for team:")
@@ -81,4 +48,3 @@ def get_point_total(homeTeam, n):
     return total
         
 print(get_point_total(team, 5))
->>>>>>> d21ded2dc00af072fceb105101f5f3ef51d1dee5
